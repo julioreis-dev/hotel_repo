5502ef7dd7db3a8dd9f27915b5b16dd0694cfa0f
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-07-04 12:21:08 -0300

    feat: add new dependencies - the files settings.py and requirements.txt received new dependencies

--- hotelAdmin/settings/settings.py
+++ hotelAdmin/settings/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -162,3 +163,5 @@ MESSAGE_TAGS = {
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
