24d2df299a2dbb08f4c2c07f91562375bf31b67f
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-07-04 14:11:28 -0300

    feat: add new dependencies - the files settings.py and requirements.txt received new dependencies

---
+++ hotelAdmin/settings/production.py
@@ -0,0 +1,14 @@
+from hotelAdmin.settings.settings import *
+import dj_database_url
+
+
+SECRET_KEY = 'django-insecure-dku(3*(n=-7=v&i-lyxedu7xn*_-9hiju^psw6476#07hj#4az'
+
+DEBUG = False
+
+# Alterar para o IP do ambiente de produção quando houver.
+ALLOWED_HOSTS = ['hotelprojec.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
