32a34be43ab7fe8e9fd366fd1e1614a69b6939db
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-07-04 18:50:15 -0300

    feat: improvements in manage.py - improvements in manage.py

--- hotelAdmin/settings/production.py
+++ hotelAdmin/settings/production.py
@@ -6,18 +6,18 @@ SECRET_KEY = 'django-insecure-dku(3*(n=-7=v&i-lyxedu7xn*_-9hiju^psw6476#07hj#4az
 DEBUG = False
 
 # Alterar para o IP do ambiente de produção quando houver.
-ALLOWED_HOSTS = ['hotelprojec.herokuapp.com']
+ALLOWED_HOSTS = ['*']
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
 
 # DATABASES = {
-#     'default': dj_database_url.config()
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
