deeb364a81fe222aea97327cb636e010dc2a0b5b
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2022-07-04 12:11:55 -0300

    feat: add new routes in urls.py - new routes were created to the project

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from rooms.views import HomepageView, HotelsListView, HotelDetailView, ReservationCreateView, MyreservationListView
+from rooms.views import RoomsUpdateView
 
 app_name = 'rooms'
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('hotel/rooms/<int:pk>/', HotelDetailView.as_view(), name='list_rooms'),
     path('hotel/rooms/reserve/<int:pk>/', ReservationCreateView.as_view(), name='reservation_rooms'),
     path('hotel/reservations/<int:pk>/', MyreservationListView.as_view(), name='my_reservations'),
+    path('hotel/update/<int:pk>/', RoomsUpdateView.as_view(), name='update_reservation'),
 ]
