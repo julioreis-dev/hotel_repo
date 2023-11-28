3b907f9293f4a5606063616fe9db81b517322713
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-11-28 19:48:50 -0300

    feat: update time_zone - The time_zone was updated to America/Sao_paulo

--- hotelAdmin/settings/settings.py
+++ hotelAdmin/settings/settings.py
@@ -100,9 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'Canada/Eastern'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
